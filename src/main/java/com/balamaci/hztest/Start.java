c6d2adcf9f4bc9bb08341432c33ae488151da02f
Serban Balamaci <balamaci@gmail.com>
2014-01-20 18:54:34 +0200

    initial commit

---
+++ src/main/java/com/balamaci/hztest/Start.java
@@ -0,0 +1,17 @@
+package com.balamaci.hztest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Serban Balamaci
+ */
+public class Start {
+
+    private static final Logger log = LoggerFactory.getLogger(Start.class);
+
+    public static void main(String[] args) throws Exception {
+        log.info("Starting client");
+    }
+
+}
