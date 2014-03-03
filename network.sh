62bcf6bea98bd27a5371dfc159df1eab83fe0250
Serban Balamaci <balamaci@gmail.com>
2014-03-03 18:46:57 +0200

    update the networking scripts

--- network.sh
+++ network.sh
@@ -6,28 +6,49 @@ N3=172.17.0.4
 N4=172.17.0.5
 
 function partition_NETWORK() {
-echo 'Partitioning N1,N2 from N3,N4
+echo 'Partitioning N1,N2 from N3,N4'
 ssh root@$N1 'bash -s' <<-ENDSSH
 	iptables -A INPUT -s $N3 -j DROP
 	iptables -A INPUT -s $N4 -j DROP
 ENDSSH
 
 ssh root@$N2 'bash -s' <<-ENDSSH
-        iptables -A INPUT -s $N3 -j DROP
-	    iptables -A INPUT -s $N4 -j DROP
+    iptables -A INPUT -s $N3 -j DROP
+	iptables -A INPUT -s $N4 -j DROP
 ENDSSH
 
-echo 'Partitioning N3,N4 from N1,N2
+echo 'Partitioning N3,N4 from N1,N2'
 ssh root@$N3 'bash -s' <<-ENDSSH
 	iptables -A INPUT -s $N1 -j DROP
 	iptables -A INPUT -s $N2 -j DROP
 ENDSSH
 
 ssh root@$N4 'bash -s' <<-ENDSSH
-        iptables -A INPUT -s $N1 -j DROP
-	    iptables -A INPUT -s $N2 -j DROP
+    iptables -A INPUT -s $N1 -j DROP
+	iptables -A INPUT -s $N2 -j DROP
 ENDSSH
 
+}
+
+function partition_N1() {
+echo 'Partitioning N1 from N2,N3,N4'
+ssh root@$N1 'bash -s' <<-ENDSSH
+    iptables -A INPUT -s $N2 -j DROP
+	iptables -A INPUT -s $N3 -j DROP
+	iptables -A INPUT -s $N4 -j DROP
+ENDSSH
+
+ssh root@$N2 'bash -s' <<-ENDSSH
+        iptables -A INPUT -s $N1 -j DROP
+ENDSSH
+
+ssh root@$N3 'bash -s' <<-ENDSSH
+	iptables -A INPUT -s $N1 -j DROP
+ENDSSH
+
+ssh root@$N4 'bash -s' <<-ENDSSH
+        iptables -A INPUT -s $N1 -j DROP
+ENDSSH
 }
 
 function heal_NETWORK() {
@@ -53,6 +74,10 @@ ENDSSH
 case "$1" in
 partition)
         partition_NETWORK;
+    ;;
+
+partition_n1)
+        partition_N1;
     ;;
 
 heal)
